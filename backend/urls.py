82bba456c4f9d5845abd5dd19ad192854dd9fdc4
Naga-Manohar-Y <yelubandimanohar155@gmail.com>
2023-11-19 15:01:04 -0500

    init

--- backend/urls.py
+++ backend/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.conf import settings # new
-from  django.conf.urls.static import static #new
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -37,6 +35,4 @@ urlpatterns = [
     path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
    
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+
